4a2fe762b52c91b54776aca96d3f43535b9d989f
fjjt3 <fjjt3@yahoo.com>
2023-07-26 12:07:21 +0200

    code(list name script added)

--- src/components/NameList.jsx
+++ src/components/NameList.jsx
@@ -1,6 +1,20 @@
 import React, {useState} from "react";
+import uniqid from 'uniqid' // not installed
 
 const NameList = () => {
+
+    const [name, setName] = useState('')
+    const [namelist, setNameslist] = useState([])
+
+    const addName = (e) => {
+        e.preventDefault()
+        const newName = {
+            id: uniqid(),
+            nameTittle: name
+        }
+        setNameslist(...namelist, newName)
+    }
+
     return(
         <div>
             <h2>CRUD APP</h2>
@@ -10,8 +24,8 @@ const NameList = () => {
                 </div>
                 <div className="col">
                     <h2>Formulary to add name</h2>
-                    <form className="form-group">
-                        <input className="form-control mb-3" type="text" placehoder="Introduce your name"/>
+                    <form onSubmit={(e)=> {addName(e)}} className="form-group">
+                        <input onChange={(e)=>{setName(e.target.value)}} className="form-control mb-3" type="text" placehoder="Introduce your name"/>
                         <input className="btn btn-info btn-block" type="submit" value="Register Name"/>
                     </form>
                 </div>
