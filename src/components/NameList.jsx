9d3c36d131dac5861bf51741d453a61cae2ec559
fjjt3 <fjjt3@yahoo.com>
2023-07-26 12:20:07 +0200

    code(added new features)

--- src/components/NameList.jsx
+++ src/components/NameList.jsx
@@ -1,37 +1,61 @@
-import React, {useState} from "react";
-import uniqid from 'uniqid' // not installed
+import React, { useState } from "react";
+import uniqid from "uniqid"; // not installed
 
 const NameList = () => {
+  const [name, setName] = useState("");
+  const [namelist, setNameslist] = useState([]);
 
-    const [name, setName] = useState('')
-    const [namelist, setNameslist] = useState([])
+  const addName = (e) => {
+    e.preventDefault();
+    const newName = {
+      id: uniqid(),
+      nameTittle: name,
+    };
+    setNameslist(...namelist, newName);
+    setName("");
+  };
 
-    const addName = (e) => {
-        e.preventDefault()
-        const newName = {
-            id: uniqid(),
-            nameTittle: name
-        }
-        setNameslist(...namelist, newName)
-    }
-
-    return(
-        <div>
-            <h2>CRUD APP</h2>
-            <div className="row">
-                <div className="col">
-                    <h2>Names List</h2>
-                </div>
-                <div className="col">
-                    <h2>Formulary to add name</h2>
-                    <form onSubmit={(e)=> {addName(e)}} className="form-group">
-                        <input onChange={(e)=>{setName(e.target.value)}} className="form-control mb-3" type="text" placehoder="Introduce your name"/>
-                        <input className="btn btn-info btn-block" type="submit" value="Register Name"/>
-                    </form>
-                </div>
-            </div>
+  return (
+    <div>
+      <h2>CRUD APP</h2>
+      <div className="row">
+        <div className="col">
+          <h2>Names List</h2>
+          <ul className="list-group">
+            {namelist.map((item) => (
+              <li key="{item.id}" className="list-group-item">
+                {item.nameTittle}
+              </li>
+            ))}
+          </ul>
         </div>
-    )
-}
+        <div className="col">
+          <h2>Formulary to add name</h2>
+          <form
+            onSubmit={(e) => {
+              addName(e);
+            }}
+            className="form-group"
+          >
+            <input
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              className="form-control mb-3"
+              type="text"
+              placehoder="Introduce your name"
+              value={name}
+            />
+            <input
+              className="btn btn-info btn-block"
+              type="submit"
+              value="Register Name"
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default NameList
+export default NameList;
