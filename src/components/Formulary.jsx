0c76aa26f46f8efd7f0e4851fca5a84a23111342
fjjt3 <fjjt3@yahoo.com>
2023-07-26 09:50:29 +0200

    code(event onChange added)

--- src/components/Formulary.jsx
+++ src/components/Formulary.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Formulary = () => {
-    return(
-        <div>
-            <form className="form-group">
-                <input placeholder="Your name" className="form-control mb-3" type="text"/>
-                <input placeholder="Your age" className="form-control mb-3" type="text"/>
-                <input className="btn btn-info btn-block mb-3" type="submit"/>
-            </form>
-        </div>
-    )
-}
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
 
-export default Formulary
+  return (
+    <div>
+      <form className="form-group">
+        <input
+          placeholder="Your name"
+          className="form-control mb-3"
+          type="text"
+          onChange={ (e) => {setName(e.target.value)}}
+        />
+        <input
+          placeholder="Your age"
+          className="form-control mb-3"
+          type="text"
+          onChange={ (e) => {setAge(e.target.value)}}
+        />
+        <input className="btn btn-info btn-block mb-3" type="submit" />
+      </form>
+    </div>
+  );
+};
+
+export default Formulary;
