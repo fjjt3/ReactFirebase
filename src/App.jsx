470c02254a4ab4df792288f75e2923dad02abf46
fjjt3 <fjjt3@yahoo.com>
2023-07-31 08:34:05 +0200

    learning(how to use props)

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Willkommen from "./components/Willkommen";
+
+function App() {
+  return (
+    <div >
+      <h2>Components Properties</h2>
+      <Willkommen name="Fran"/>
+      <Willkommen name="Manuel"/>
+    </div>  
+  );
+}
+
+export default App;
